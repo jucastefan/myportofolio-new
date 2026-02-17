2892907d5cb661d585c105b220c12bc3933d85d4
jucastefan <juca_stefan_florin@yahoo.com>
2026-02-17 18:15:36 +0200

    update

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ */
+
+export default {
+	async fetch(request: Request): Promise<Response> {
+		return fetch(request);
+	},
+} satisfies ExportedHandler<Env>;
+
+interface Env {}
+
+export interface ExportedHandler<Env = unknown> {
+	fetch: ExportedHandlerFetchHandler<Env>;
+}
+
+export type ExportedHandlerFetchHandler<Env = unknown> = (
+	request: Request,
+	env: Env,
+	ctx: ExecutionContext,
+) => Response | Promise<Response>;
+
+interface ExecutionContext {
+	waitUntil(promise: Promise<any>): void;
+	passThroughOnException(): void;
+}
